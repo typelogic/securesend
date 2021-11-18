01eb5bffecd7349cedca707535f07992e846bbcc
Reuben Mangubat <reuben@newlogic.com>
2021-11-18 14:05:12 +0800

    Add fix for permissions not showing in Android 11 devices

--- android/app/src/main/java/com/securesend/MainActivity.java
+++ android/app/src/main/java/com/securesend/MainActivity.java
@@ -14,93 +14,94 @@ import androidx.core.content.ContextCompat;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
-  private static final String[] REQUIRED_PERMISSIONS =
-    new String[]{
-      Manifest.permission.BLUETOOTH,
-      Manifest.permission.BLUETOOTH_ADMIN,
-      Manifest.permission.ACCESS_WIFI_STATE,
-      Manifest.permission.CHANGE_WIFI_STATE,
-      Manifest.permission.READ_EXTERNAL_STORAGE,
-      Manifest.permission.WRITE_EXTERNAL_STORAGE,
-      Manifest.permission.ACCESS_COARSE_LOCATION,
-      Manifest.permission.ACCESS_FINE_LOCATION,
-    };
 
-  private static final String[] REQUIRED_PERMISSIONS_Q =
-    new String[]{
-      Manifest.permission.BLUETOOTH,
-      Manifest.permission.BLUETOOTH_ADMIN,
-      Manifest.permission.ACCESS_WIFI_STATE,
-      Manifest.permission.CHANGE_WIFI_STATE,
-      Manifest.permission.READ_EXTERNAL_STORAGE,
-      Manifest.permission.WRITE_EXTERNAL_STORAGE,
-      Manifest.permission.ACCESS_COARSE_LOCATION,
-      Manifest.permission.ACCESS_FINE_LOCATION,
-      Manifest.permission.ACCESS_BACKGROUND_LOCATION
-    };
+    private static final String TAG = MainActivity.class.getName();
 
-  private static final int REQUEST_CODE_REQUIRED_PERMISSIONS = 1;
+    private static final String[] REQUIRED_PERMISSIONS =
+            new String[]{
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.ACCESS_WIFI_STATE,
+                    Manifest.permission.CHANGE_WIFI_STATE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+            };
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "securesend";
-  }
+    private static final String[] REQUIRED_PERMISSIONS_BACKGROUND = new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION};
+    
+    private static final int REQUEST_CODE_REQUIRED_PERMISSIONS = 1;
 
-  @RequiresApi(api = Build.VERSION_CODES.M)
-  @Override
-  protected void onStart() {
-    super.onStart();
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-      if (!hasPermissions(this, REQUIRED_PERMISSIONS_Q)) {
-        requestPermissions(REQUIRED_PERMISSIONS_Q, REQUEST_CODE_REQUIRED_PERMISSIONS);
-      }
-    } else {
-      if (!hasPermissions(this, REQUIRED_PERMISSIONS)) {
-        requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_REQUIRED_PERMISSIONS);
-      }
-    }
-  }
-
-  /**
-  * Returns true if the app was granted all the permissions. Otherwise, returns false.
-  */
-  private static boolean hasPermissions(Context context, String... permissions) {
-    for (String permission : permissions) {
-      if (ContextCompat.checkSelfPermission(context, permission)
-          != PackageManager.PERMISSION_GRANTED) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  /**
-  * Handles user acceptance (or denial) of our permission request.
-  */
-  @CallSuper
-  @Override
-  public void onRequestPermissionsResult(
-      int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-  {
-    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-    if (requestCode != REQUEST_CODE_REQUIRED_PERMISSIONS) {
-      return;
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "securesend";
     }
 
-    for (int grantResult : grantResults) {
-      if (grantResult == PackageManager.PERMISSION_DENIED) {
-        // Toast.makeText(this, R.string.error_missing_permissions, Toast.LENGTH_LONG).show();
-        // connectButton.setEnabled(false);
-        Log.d("Main", "Denied");
-        return;
-      }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            Log.d(TAG, "*** Permissions check ---> Android Q ***");
+            if (!hasPermissions(this, REQUIRED_PERMISSIONS)) {
+                this.requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_REQUIRED_PERMISSIONS);
+            } else {
+                Log.d(TAG, "*** Permissions check ---> Background Location ***");
+                int isBackgroundPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION);
+                if (isBackgroundPermission != PackageManager.PERMISSION_GRANTED) {
+                    this.requestPermissions(REQUIRED_PERMISSIONS_BACKGROUND, REQUEST_CODE_REQUIRED_PERMISSIONS);
+                }
+            }
+        } else {
+            if (!hasPermissions(this, REQUIRED_PERMISSIONS)) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    Log.d(TAG, "*** Permissions check ---> Android M ***");
+                    this.requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_REQUIRED_PERMISSIONS);
+                }
+            }
+        }
     }
-    recreate();
-  }
+
+    /**
+     * Returns true if the app was granted all the permissions. Otherwise, returns false.
+     */
+    private static boolean hasPermissions(Context context, String... permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(context, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Handles user acceptance (or denial) of our permission request.
+     */
+    @CallSuper
+    @Override
+    public void onRequestPermissionsResult(
+        int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode != REQUEST_CODE_REQUIRED_PERMISSIONS) {
+            return;
+        }
+
+        for (int grantResult : grantResults) {
+            if (grantResult == PackageManager.PERMISSION_DENIED) {
+                // Toast.makeText(this, R.string.error_missing_permissions, Toast.LENGTH_LONG).show();
+                // connectButton.setEnabled(false);
+                Log.d(TAG, "Denied");
+                return;
+            }
+        }
+        recreate();
+    }
 }
